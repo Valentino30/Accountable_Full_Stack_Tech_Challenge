1c1cb85c8fb84302993e1d34a51bcf98493932c3
Valentino Giudice <giudicevalentino@gmail.com>
2025-09-10 19:19:53 +0800

    feat(user): add User model for registration and login

---
+++ backend/src/models/User.ts
@@ -0,0 +1,14 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface IUser extends Document {
+  email: string;
+  password: string;
+  _id: Schema.Types.ObjectId;
+}
+
+const UserSchema = new Schema<IUser>({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+export default model<IUser>("User", UserSchema);
