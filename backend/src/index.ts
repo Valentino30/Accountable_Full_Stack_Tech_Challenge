a98a4b36a086c78ed6e4eb5d7f5436c172148ae6
Valentino Giudice <giudicevalentino@gmail.com>
2025-09-10 14:59:58 +0800

    chore(git): stop tracking .env file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/events", eventRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const start = async () => {
   try {
