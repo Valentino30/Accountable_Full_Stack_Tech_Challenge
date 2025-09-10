8d9b67b4ef6787228fb14af2e1667381fb9b1802
Valentino Giudice <giudicevalentino@gmail.com>
2025-09-10 14:43:16 +0800

    feat(event): add totalSpots and reservedSpots to Event model for reservations

--- backend/src/models/Event.ts
+++ backend/src/models/Event.ts
@@ -5,6 +5,8 @@ export interface IEvent extends Document {
   country: string;
   date: Date;
   team: string;
+  totalSpots: number;
+  reservedSpots: number;
 }
 
 const EventSchema = new Schema<IEvent>(
@@ -13,6 +15,8 @@ const EventSchema = new Schema<IEvent>(
     country: { type: String, required: true },
     date: { type: Date, required: true },
     team: { type: String, required: true },
+    totalSpots: { type: Number, required: true, min: 0, default: 100 }, // default can be anything
+    reservedSpots: { type: Number, required: true, min: 0, default: 0 },
   },
   { timestamps: true }
 );
