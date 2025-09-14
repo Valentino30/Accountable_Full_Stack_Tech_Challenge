538a0c03e251aeeac62ddd3c09e906dc893b79b5
Valentino Giudice <giudicevalentino@gmail.com>
2025-09-14 12:21:34 +0800

    feat(auth): update auth routes

--- mobile/api/auth.ts
+++ mobile/api/auth.ts
@@ -2,16 +2,16 @@ import apiClient from "./apiClient";
 import type { LoginParams, LoginResponse, RegisterParams, RegisterResponse, RefreshTokenResponse } from "../types/auth";
 
 export const loginUser = async ({ email, password }: LoginParams): Promise<string> => {
-  const res = await apiClient.post<LoginResponse>("/users/login", { email, password });
+  const res = await apiClient.post<LoginResponse>("/auth/login", { email, password });
   return res.data.token;
 };
 
 export const registerUser = async ({ email, password }: RegisterParams): Promise<string> => {
-  const res = await apiClient.post<RegisterResponse>("/users/register", { email, password });
+  const res = await apiClient.post<RegisterResponse>("/auth/register", { email, password });
   return res.data.token;
 };
 
 export const refreshToken = async (): Promise<string> => {
-  const res = await apiClient.post<RefreshTokenResponse>("/users/refresh-token");
+  const res = await apiClient.post<RefreshTokenResponse>("/auth/refresh-token");
   return res.data.token;
 };
