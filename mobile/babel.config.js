b58e865eca52d3edadd8968a53ba2241315f0ecc
Valentino Giudice <giudicevalentino@gmail.com>
2025-09-16 13:18:11 +0800

    chore(test): update Babel, Jest configs and dependencies for test compatibility

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,11 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo", "@babel/preset-env", "@babel/preset-typescript"],
-    plugins: [
-      ["@babel/plugin-transform-class-properties", { loose: true }],
-      ["@babel/plugin-transform-private-methods", { loose: true }],
-      ["@babel/plugin-transform-private-property-in-object", { loose: true }],
-    ],
+    presets: ["babel-preset-expo"],
   };
 };
--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: "jest-expo",
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
   transformIgnorePatterns: [
-    "node_modules/(?!(react-native|@react-native|@react-navigation|expo|@expo|expo-modules-core|@expo/vector-icons|expo-font)/)",
+    "node_modules/(?!(react-native|@react-native|react-navigation|expo|@expo|expo-modules-core|@expo/vector-icons|expo-font|react-native-dropdown-picker)/)",
   ],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
